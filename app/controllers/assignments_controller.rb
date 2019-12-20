915bb74796d75bb6e2c84c127d9e5cb756f8be2c
My Koryto <mykoryto93@gmail.com>
2019-12-20 05:05:36 +0200

    INCOMPLETE assignment controller modification

--- app/controllers/assignments_controller.rb
+++ app/controllers/assignments_controller.rb
@@ -1,7 +1,32 @@
 class AssignmentsController < ApplicationController
   def index
+    @assignment = Assignment.first(7)
   end
 
   def show
+    @assignment = Assignment.find(params[:id])
+  end
+
+  def new
+    @assignment = Assignment.new
+  end
+
+  def create
+    @assignment = Assignment.new(params[:assignment])    
+    @assignment.save
+  end
+
+  def delete
+    @assignment = Assignment.find(params[:id])
+  end
+
+  def destroy
+    @assignment = Assignment.find(params[:id])
+    @assignment.destroy
+  end
+
+  private
+  def assignment_params    
+    params.require(:assignment).permit(:name, :due_time, :description)
   end
 end
